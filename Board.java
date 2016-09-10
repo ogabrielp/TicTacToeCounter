4c8ddfdb6cdbe1b2444c87ae1e873d960b48c09f
Gabriel Pinheiro <gabrielpcosta@gmail.com>
2016-09-10 12:59:22 -0300

    Added Board.java
    Board.java is an auxiliary class, which will be used to improve
    legibility.

---
+++ Board.java
@@ -0,0 +1,8 @@
+/* Board.java
+ *
+ * This is an auxiliary class in order to further increase legibility of
+ * the program.
+ * I am well aware that some of my notation choices ended up taking more
+ * lines of code than it would be necessary. However, I decided to choose
+ * a legible code over a compact one.
+ */
