2baad96a925f244957715c45e524eca6d34244b1
Gabriel Pinheiro <gabrielpcosta@gmail.com>
2016-09-19 18:53:08 -0300

    Finished project
    Added the additional methods which were required to determine if a board
    is valid and modified the main class to verify the amount of valid
    possible outcomes.

--- Board.java
+++ Board.java
@@ -145,6 +145,9 @@ class Board
   /*
    * The set() method changes the current state of the board based on a
    * string passed as a parameter.
+   *
+   * Parameters:
+   * - String input: the input string to be converted to a board.
    */
 
   void set(String input)
@@ -178,23 +181,191 @@ class Board
    */
   String visualize()
   {
-    String output = "|";                
-    for (int i = 0; i < 9; i++)         //Goes through the array
+    String output = "";                   // Initializes the output string
+    for (int i = 0; i < ARRAY_SIZE; i++)  // Goes through the array
     {
-      if (i % 3 == 0 && i != 0)         //Every three positions
-      {
-        output += "\n|";                //Inserts a line break
-      }
+      if (i % 3 == 0 && i != 0)           // Every three positions
+        output += "\n";                   // Inserts a line break
 
-      switch(array[i])
-      {
-        case 0:   output += " |";break;
-        case 1:   output += "X|";break;
-        case 2:   output += "O|";break;
-        default:  output += "?|";break;
+      /* It is necessary to add the "i != 0" restriction, because otherwise
+       * the algorithm would insert a line break at the beginning of the string,
+       * which I did not want. (didn't want to substring either)
+       */
+
+      switch (array[i])                   // This switch appends the equivalent
+      {                                   // character to the output string
+        case 0:   output += " ";break;    // based on the array value.
+        case 1:   output += "X";break;
+        case 2:   output += "O";break;
+        default:  output += "?";break;
       }
     }
 
     return output;
+  }
+
+  /* Here we start creating the functions that will actually be used to
+   * perform the calculations.
+   */
+
+  /* For a final game state to be valid, we need to check for some criteria:
+   * - The absolute value of the difference between Xs and Os must be either
+   * 1 (if the first player won or if the match ended up in a draw) or 0 (if
+   * the second player won);
+   * - If there are empty spaces on the board, someone must have won; otherwise,
+   * the game is not over yet, and therefore, it does not qualify as a final
+   * game state;
+   * - Only one of the players may have won;
+   * - The player can't have won twice in the same board.
+   *
+   * Here, we have the isValidGame(), which checks each one of theses
+   * possibilites, using methods declared down below.
+   *
+   * It returns TRUE if the board state meets all of the aforementioned
+   * criteria, and FALSE if it does not.
+   */
+  boolean isValidGame()
+  {
+    int difference = Math.abs(amountOf(X) - amountOf(O));
+
+    if  (!(difference == 1 || difference == 0) ||
+        (hasEmptySpaces() && hasWon(X) != 1 && hasWon(O) != 1) ||
+        (hasWon(X) != hasWon(O)) ||
+        (hasWon(X) == -2 || hasWon(O) == -2))
+      return false;
+
+    return true;
+  }
+
+  /* The amountOf() method determines the amount of occurrences of a particular
+   * number in the array.
+   *
+   * Parameters:
+   * - int number: the integer value of one of the three possible symbols for
+   * the board, according to the constants declared at the beginning of the
+   * file.
+   *
+   * It returns the amount of occurrences of that number in the array.
+   */
+  int amountOf(int number)
+  {
+    int amount = 0;                     // Initializing return variable
+
+    for (int i = 0; i < ARRAY_SIZE; i++) // Goes through the array
+    {
+      if (array[i] == number)            // If the number is found in the array
+        amount++;                       // Increases return variable
+    }
+
+    return amount;                      // Returns the amount of occurrences
+  }
+
+  /* The hasEmptySpaces() method examines the board to check if there are
+   * any blank positions.
+   *
+   * It returns TRUE if there is ANY blank space, FALSE otherwise.
+   */
+  boolean hasEmptySpaces()
+  {
+    for (int i = 0; i < ARRAY_SIZE; i++)  // Goes through the array
+    {
+      if (array[i] == BLANK)              // If it finds a blank position
+        return true;                      // Returns true
+    }
+
+    return false;                         // Returns false otherwise
+  }
+
+  /*
+   * The hasWon() method verifies if a particular player has won the match.
+   *
+   * Parameters:
+   * - int number: the integer value of either X or O, according to the
+   * constants defined at the beginning of the file.
+   *
+   * It returns:
+   * 1, if the player won;
+   * 0, if the player did not win;
+   * -1, if the input is invalid;
+   * -2, if the player won multiple times in the same board.
+   *
+   * For better understanding of the algorithm below, please consider this
+   * association:
+   * - array: 012 345 678, where the numbers are the positions. This will be
+   * processed as:
+   * 0 1 2
+   * 3 4 5
+   * 6 7 8
+   */
+  int hasWon(int number)
+  {
+    int verification = 0; // This is a control variable, it will be used to
+                          // determine which value will be returned.
+
+    // If the input is neither X or O, it is invalid, therefore, -1 is returned
+    if (!(number ==  X || number == O))
+      return -1;
+
+    // This scans the lines of the board
+    for (int i = 0; i < ARRAY_SIZE; i += 3)
+    {
+      // Compares the left, center and right positions of the row with the
+      // number received as an argument
+      if (areAllEqual(array[i], array[i+1], array[i+2], number))
+        verification++;
+    }
+
+    // This scans the columns of the board
+    for (int i = 0; i < 3; i++)
+    {
+      // Compares the top, middle, and bottom positions of the column with the
+      // number received as an argument
+      if (areAllEqual(array[i], array[i+3], array[i+6], number))
+        verification++;
+    }
+
+    // Checks the diagonal that spans from the upper-left corner to the
+    // bottom-right corner with the number received as an argument
+    if (areAllEqual(array[0], array[4], array[8], number))
+      verification++;
+
+    // Checks the diagonal that spans from the upper-right corner to the
+    // bottom-left corner with the number received as an argument
+    if (areAllEqual(array[2], array[4], array[6], number))
+      verification++;
+
+    /* If verification:
+     * >>> equals 1, only one of the criteria was met, and therefore,
+     * the player won the game in a valid manner; 1 itself is returned.
+     * >>> equals 0, none of the criteria was met, and therefore, the player
+     * did not win the game; 0 itself is returned.
+     * >>> equals any other value, the player won multiple times on the same
+     * board, which is invalid; -2 is returned.
+     */
+    return (verification == 1 || verification == 0) ? verification : -2;
+  }
+
+  /* The areAllEqual() method is a triple comparator. I decided to create such
+   * method because I do triple comparations a lot in the hasWon() method, and
+   * the code ended up really cluttered, so, I decided to clean things up.
+   *
+   * if (array[i] == array[i+1] && array[i+1] == array[1+2] && array[i] == n)
+   *
+   * became
+   *
+   * if(areAllEqual(array[i], array[i+1], array[i+2], n))
+   *
+   * It returns TRUE if all of the parameters received are equal to one another,
+   * FALSE otherwise.
+   */
+  boolean areAllEqual(int... numbers)
+  {
+    for (int i = 0; i < numbers.length; i++)
+    {
+      if(numbers[i] != numbers[0])
+        return false;
+    }
+
+    return true;
   }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -12,26 +12,11 @@ public class TicTacToe
     while (!b.isFinished())
     {
       // Count and increase the value in b
-      count++;
+      if (b.isValidGame())
+        count++;
       b.increase();
     }
 
-    // This last count is here because the while loop does not count the last
-    // value (222 222 222) since it does not repeat the loop once b reaches
-    // the maximum value.
-    count++;
-
-    // Output
     System.out.println(count);
-
-    //Testing the new methods: this is a valid input and should work
-    b.set("102020101");
-    System.out.println("Board: \n"+b.visualize());
-
-    // This is not because it has a 3 in it
-    b.set("130000000");
-
-    // This is not because it has the letter O in it
-    b.set("000000O00");
   }
 }
